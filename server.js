a54a52af1ba04b06b18a7eb28c27c17555776cb6
jmpak <jmpak80@gmail.com>
2012-07-30 11:21:02 +0800

    basic login screen done

---
+++ server.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+require('coffee-script')
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', { layout: true });
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+require('./apps/authentication/routes')(app)
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
